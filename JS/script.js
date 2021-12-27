90322300f08b15578feed13a78a0b4001c330c5a
Vitaly <vitoss2308@yandex.ru>
2021-12-28 00:23:55 +0300

    first_commit

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+'use strict'
+// let number = 10;
+// let max = Number.MAX_SAFE_INTEGER;
+// number = 5;
+// max=BigInt(max)+208n;
+// console.log(max);
+// alert('hello')
+// const result = confirm('Are you hear?');
+// const answer = +prompt('тебе есть 18?');
+// console.log(answer + 3);
+
+// const answers = [];
+//
+// answers[0] = prompt('Как имя?');
+// answers[1] = prompt('Як фамилия?');
+// answers[2] = prompt('Сколько рокiв?');
+//
+// document.write(answers)
+// console.log(typeof (answers));
+//   const data = 'Павел Федорович';
+// alert(`Привет ${data}`)
+
+let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+console.log(incr);
+console.log(decr);
+console.log(6%4);
